a495056249b305ade75a9b95dd7f87924927ed76
Nikola Vrhovac <vrllee@gmail.com>
2021-10-17 15:01:58 +0200

    add react routing

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const activeStyle = ({ isActive }) => ({ color: isActive && 'purple' });
 
 export default function Header() {
 	return (
@@ -6,7 +9,19 @@ export default function Header() {
 			<nav>
 				<ul>
 					<li>
-						<img alt='Carved Rock Fitness' src='/images/logo.png' />
+						<Link to='/'>
+							<img alt='Carved Rock Fitness' src='/images/logo.png' />
+						</Link>
+					</li>
+					<li>
+						<NavLink style={activeStyle} to='/shoes'>
+							Shoes
+						</NavLink>
+					</li>
+					<li>
+						<NavLink style={activeStyle} to='/cart'>
+							Cart
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
