e662a7c6c37e8b150e58eb29cfff7f4b8e132b9e
Nikola Vrhovac <vrllee@gmail.com>
2021-10-18 22:01:54 +0200

    form validation

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import useFetchAll from './services/useFetchAll';
+import { useNavigate } from 'react-router-dom';
 import Spinner from './Spinner';
 
 export default function Cart({ cart, updateQuantity }) {
+	const navigate = useNavigate();
 	const urls = cart.map(i => `products/${i.id}`);
 	const { data: products, loading, error } = useFetchAll(urls);
 
@@ -51,6 +53,13 @@ export default function Cart({ cart, updateQuantity }) {
 					: `${numItemsInCart} Item${numItemsInCart > 1 ? 's' : ''} in My Cart`}
 			</h1>
 			<ul>{cart.map(renderItem)}</ul>
+			{cart.length > 0 && (
+				<button
+					className='btn btn-primary'
+					onClick={() => navigate('/checkout')}>
+					Checkout
+				</button>
+			)}
 		</section>
 	);
 }
